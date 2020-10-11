c3279810e829545213042df88ef7d241503fe0ca
Constantin <constantinu93@gmail.com>
2020-10-11 12:14:18 +0200

    add event-listener to clearBtn and decimal

--- script.js
+++ script.js
@@ -8,6 +8,13 @@ const sendNumberValue = (number) => { /* send the clicked Button to the calculat
     calculatorDisplay.textContent = displayValue === '0' ? number : displayValue + number;
 }
 
+const addDecimal = () => {
+    // If no decimal, add one. If there already is one, don't add another:
+    if (!calculatorDisplay.textContent.includes('.')) {
+        calculatorDisplay.textContent = `${calculatorDisplay.textContent}.`;
+    }
+}
+
 // ========== Add Event Listeners for numbers, operators, decimal buttons ==========
 inputBtns.forEach((inputBtn) => {
     if (inputBtn.classList.length === 0) {
@@ -15,6 +22,14 @@ inputBtns.forEach((inputBtn) => {
     } else if (inputBtn.classList.contains('operator')) {
         inputBtn.addEventListener('click', () => sendNumberValue(inputBtn.value)); /* add Event Listeners for the operators */
     } else if (inputBtn.classList.contains('decimal')) {
-        inputBtn.addEventListener('click', () => sendNumberValue(inputBtn.value)); /* add Event Listeners for the decimal */
+        inputBtn.addEventListener('click', () => addDecimal()); /* add Event Listeners for the decimal */
     }
 });
+
+// ========== Reset display ==========
+const resetAll = () => {
+    calculatorDisplay.textContent = '0';
+}
+
+// Event Listener
+clearBtn.addEventListener('click', resetAll);
